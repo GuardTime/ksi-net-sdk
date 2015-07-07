f7be83130fce9bf76ee3a101fe96ff844247a0c0
Martti Marran <martti.marran@guardtime.com>
2015-07-07 19:05:02 +0300

    KSINETAPI-8 Removed comment from PKI trust provider

--- ksi-net-api/Trust/PkiTrustStoreProvider.cs
+++ ksi-net-api/Trust/PkiTrustStoreProvider.cs
@@ -19,12 +19,13 @@ namespace Guardtime.KSI.Trust
             {
                 throw new ArgumentNullException("x509SignatureBytes");
             }
+            
 
             // TODO: Java API email verification does not check email correctly, if its missing then it skips it
             SignedCms signedCms = new SignedCms(new ContentInfo(signedBytes), true);
             signedCms.Decode(x509SignatureBytes);
             signedCms.CheckSignature(false);
-            // TODO: Check the email
+
             Console.WriteLine(signedCms.SignerInfos[0].Certificate.GetNameInfo(X509NameType.EmailName, false));
         }
     }
