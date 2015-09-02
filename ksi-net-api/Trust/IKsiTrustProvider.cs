0cb1633aa22e97706b1e36fbe6de07550550446d
Martti Marran <martti.marran@guardtime.com>
2015-09-02 16:57:47 +0300

    KSINETAPI-7 commented functionality

--- ksi-net-api/Trust/IKsiTrustProvider.cs
+++ ksi-net-api/Trust/IKsiTrustProvider.cs
@@ -3,12 +3,24 @@ using Guardtime.KSI.Publication;
 
 namespace Guardtime.KSI.Trust
 {
+    /// <summary>
+    /// KSI trust provider interface.
+    /// </summary>
     public interface IKsiTrustProvider
     {
+        /// <summary>
+        /// KSI trust provider contains given publication record.
+        /// </summary>
+        /// <param name="publicationRecord">publication record</param>
+        /// <returns>true if publication record exists in ksi trust provider</returns>
         bool Contains(PublicationRecord publicationRecord);
+        /// <summary>
+        /// Find X509 certificate in KSI trust provider.
+        /// </summary>
+        /// <param name="certificateId">certificate ID</param>
+        /// <returns>x509 certificate contained in trust provider</returns>
         X509Certificate FindCertificateById(byte[] certificateId);
-        // TODO: Create verification
-//        VerificationResult GetVerificationResult();
+
 
     }
 }
