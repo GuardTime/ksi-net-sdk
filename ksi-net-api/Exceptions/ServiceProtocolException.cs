88514ca9f5c631bbd83adfb53672dc2209007779
Martti Marran <martti.marran@guardtime.com>
2015-09-07 16:14:26 +0300

    KSINETAPI-14 Created service protocol exception for KSI service protocols

---
+++ ksi-net-api/Exceptions/ServiceProtocolException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Guardtime.KSI.Exceptions
+{
+    /// <summary>
+    /// KSI API base exception
+    /// </summary>
+    public class ServiceProtocolException : KsiException
+    {
+        /// <summary>
+        /// Create new KSI exception
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public ServiceProtocolException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Create new KSI exception
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Inner exception</param>
+        public ServiceProtocolException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
