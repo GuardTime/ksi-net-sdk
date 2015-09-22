af6dde18d73203b27b23c14f545aab00450f02b1
Martti Marran <martti.marran@guardtime.com>
2015-09-22 16:41:39 +0300

    KSINETAPI-9 added tests for signature verifier

---
+++ ksi-net-api-test/Signature/Verification/Rule/AggregationChainInputHashVerificationVerificationRuleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Guardtime.KSI.Signature.Verification.Rule
+{
+    // Better name for ruletests
+    [TestFixture]
+    public class AggregationChainInputHashVerificationVerificationRuleTests
+    {
+        [Test]
+        public void TestVerify()
+        {
+            var rule = new AggregationChainInputHashVerificationRule();
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                rule.Verify(null);
+            });
+
+            using (var stream = new FileStream(Properties.Resources.KsiSignatureDo_Ok, FileMode.Open))
+            {
+                var context = new TestVerificationContext()
+                {
+                    Signature = KsiSignature.GetInstance(stream)
+                };
+
+                rule.Verify(context);
+            }
+        }
+    }
+}
--- ksi-net-api-test/Signature/Verification/SignatureVerificationTests.cs
+++ ksi-net-api-test/Signature/Verification/SignatureVerificationTests.cs
@@ -23,9 +23,8 @@ namespace Guardtime.KSI.Signature.Verification
                     "http://verify.guardtime.com/ksi-publications.bin");
 
                 var ksiService = new KsiService(serviceProtocol, serviceProtocol, serviceProtocol, new ServiceCredentials("anon", "anon"), new PublicationsFileFactory());
-                var context = new VerificationContext
+                var context = new VerificationContext(KsiSignature.GetInstance(stream))
                 {
-                    Signature = KsiSignature.GetInstance(stream),
                     DocumentHash =
                         new Hashing.DataHash(new byte[]
                         {
@@ -34,26 +33,22 @@ namespace Guardtime.KSI.Signature.Verification
                             0xDC, 0xAD, 0xB2, 0x38, 0x55, 0x2D, 0x86, 0xC6, 0x59, 0x34, 0x2D, 0x1D, 0x7E, 0x87, 0xB8,
                             0x77, 0x2D
                         }),
-                    UserPublication = new PublicationData("AAAAAA-CVZ2AQ-AAIVXJ-PLJDAG-JMMYUC-OTP2GA-ELBIDQ-OKDY3C-C3VEH2-AR35I2-OJUACP-GOGD6K"),
-                    ExtendingAllowed = true,
+                    //UserPublication = new PublicationData("AAAAAA-CVZ2AQ-AAIVXJ-PLJDAG-JMMYUC-OTP2GA-ELBIDQ-OKDY3C-C3VEH2-AR35I2-OJUACP-GOGD6K"),
+                    IsExtendingAllowed = true,
                     KsiService = ksiService,
                     // TODO: use resource for publications file
                     PublicationsFile = PublicationsFile.GetInstance(new FileStream("resources/publication/publicationsfile/ksi-publications.bin", FileMode.Open))
                 };
 
                 Console.WriteLine(@"// Internal verification policy");
-                IPolicy policy = new InternalVerificationPolicy();
+                VerificationPolicy policy = new InternalVerificationPolicy();
                 policy.Verify(context);
 
-                Console.WriteLine(@"// Publication");
-                policy = new PublicationVerificationPolicy();
+                Console.WriteLine(@"// Publications based");
+                policy = new PublicationBasedVerificationPolicy();
                 policy.Verify(context);
 
-                Console.WriteLine(@"// Publications file");
-                policy = new PublicationsFileVerificationPolicy();
-                policy.Verify(context);
-
-                Console.WriteLine(@"// PKI");
+                Console.WriteLine(@"// Key based");
                 policy = new KeyBasedVerificationPolicy();
                 policy.Verify(context);
 
