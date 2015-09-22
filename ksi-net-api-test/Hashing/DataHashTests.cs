36f664518f65beac8ee294a802efa5292ce408d1
Martti Marran <martti.marran@guardtime.com>
2015-09-22 16:36:32 +0300

    KSINETAPI-19 fixed tests so they wont fail on new version of nunit

--- ksi-net-api-test/Hashing/DataHashTests.cs
+++ ksi-net-api-test/Hashing/DataHashTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 
 namespace Guardtime.KSI.Hashing
@@ -31,52 +32,85 @@ namespace Guardtime.KSI.Hashing
         {
             var dataHash = new DataHash(new byte[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 });
             Assert.IsTrue(dataHash.Equals(new DataHash(new byte[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 })), "Hash should be equal to itself");
+            Assert.IsFalse(dataHash.Equals(new ChildDataHash(new byte[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 })));
             Assert.IsFalse(dataHash.Equals(null), "Hash should not equal to null");
             Assert.IsFalse(dataHash.Equals(new object()), "Hash should not equal to empty object");
             Assert.AreEqual(new DataHash(HashAlgorithm.Sha2256, new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}), dataHash, "Hash should be equal to similar hash algorithm and value");
             Assert.AreEqual("SHA-256:[0102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F20]", dataHash.ToString(), "Hash string representation should be in correct format");
         }
 
-        [Test, ExpectedException(typeof(ArgumentNullException))]
+        [Test]
         public void TestDataHashCreateWithNullAlgorithm()
         {
-            var dataHash = new DataHash(null, new byte[] {});
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                new DataHash(null, new byte[] {});
+            });
         }
 
-        [Test, ExpectedException(typeof(ArgumentNullException))]
+        [Test]
         public void TestDataHashCreateWithNullValue()
         {
-            var dataHash = new DataHash(HashAlgorithm.Sha2256, null);
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                new DataHash(HashAlgorithm.Sha2256, null);
+            });
         }
 
-        [Test, ExpectedException(typeof(FormatException))]
+        [Test]
         public void TestDataHashCreateWithInvalidValueLength()
         {
-            var dataHash = new DataHash(HashAlgorithm.Sha2256, new byte[] {});
+            Assert.Throws<FormatException>(delegate
+            {
+                new DataHash(HashAlgorithm.Sha2256, new byte[] {});
+            }, "Hash size(0) does not match SHA-256 size(32)");
         }
 
-        [Test, ExpectedException(typeof(ArgumentNullException))]
+        [Test]
         public void TestDataHashCreateWithNullBytes()
         {
-            var dataHash = new DataHash(null);
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                new DataHash(null);
+            });
         }
 
-        [Test, ExpectedException(typeof(ArgumentException))]
+        [Test]
         public void TestDataHashCreateWithZeroLengthBytes()
         {
-            var dataHash = new DataHash(new byte[] { });
+            Assert.Throws<ArgumentException>(delegate
+            {
+                new DataHash(new byte[] {});
+            });
         }
 
-        [Test, ExpectedException(typeof(FormatException))]
+        [Test]
         public void TestDataHashCreateWithInvalidAlgorithmFromBytes()
         {
-            var dataHash = new DataHash(new byte[] { 255 });
+            Assert.Throws<FormatException>(delegate
+            {
+                new DataHash(new byte[] { 255 });
+            }, "Hash algorithm ID unknown: 255");
         }
 
-        [Test, ExpectedException(typeof(FormatException))]
+        [Test]
         public void TestDataHashCreateWithInvalidValueLengthFromBytes()
         {
-            var dataHash = new DataHash(new byte[] { 1, 1, 2, 3, 4, 5, 6 });
+            Assert.Throws<FormatException>(delegate
+            {
+                new DataHash(new byte[] { 1, 1, 2, 3, 4, 5, 6 });
+            }, "Hash size(6) does not match SHA-256 size(32)");
+        }
+
+        private class ChildDataHash : DataHash
+        {
+            public ChildDataHash(HashAlgorithm algorithm, byte[] valueBytes) : base(algorithm, valueBytes)
+            {
+            }
+
+            public ChildDataHash(byte[] imprintBytes) : base(imprintBytes)
+            {
+            }
         }
     }
 }
