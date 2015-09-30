2236573a31396418f80075dea2989a298cb86aa7
Martti Marran <martti.marran@guardtime.com>
2015-09-30 15:11:09 +0300

    KSINETAPI-20 fixed problem with double disposing streams, also created interface for TLV to write them to stream.

---
+++ ksi-net-api/Parser/ITlvTag.cs
@@ -0,0 +1,29 @@
+﻿namespace Guardtime.KSI.Parser
+{
+    /// <summary>
+    ///     TLV objects base class.
+    /// </summary>
+    public interface ITlvTag
+    {
+        /// <summary>
+        ///     Tlv tag type.
+        /// </summary>
+        uint Type { get; }
+
+        /// <summary>
+        ///     Is tlv tag non critical.
+        /// </summary>
+        bool NonCritical { get; }
+
+        /// <summary>
+        ///     Is tlv forwarded.
+        /// </summary>
+        bool Forward { get; }
+
+        /// <summary>
+        ///     Encode TLV object value.
+        /// </summary>
+        /// <returns>TLV object value as bytes</returns>
+        byte[] EncodeValue();
+    }
+}
