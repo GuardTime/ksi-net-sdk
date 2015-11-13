e75a8db180ceea5edcad0cdc80718856eb9366fd
tarmo.paavel <tarmo.paavel@gmail.com>
2015-11-13 11:31:47 +0200

    KSINETAPI-22 Replaced Dictionary in ICryptoSignatureVerifier

---
+++ ksi-net-api/Crypto/CryptoSignatureVerificationData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Guardtime.KSI.Crypto
+{
+    public class CryptoSignatureVerificationData
+    {
+        public X509Certificate2 Certificate { get; set; }
+
+        public string DigestAlgorithm { get; set; }
+
+        public CryptoSignatureVerificationData(X509Certificate2 certificate, string digestAlgorithm)
+        {
+            Certificate = certificate;
+            DigestAlgorithm = digestAlgorithm;
+        }
+    }
+}
