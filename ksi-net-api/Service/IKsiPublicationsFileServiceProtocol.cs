762e5fed909eaeba0d11e2aaaef3630948ae6feb
tarmo.paavel <tarmo.paavel@guardtime.com>
2017-03-23 13:20:35 +0200

    Fixed some comments

--- ksi-net-api/Service/IKsiPublicationsFileServiceProtocol.cs
+++ ksi-net-api/Service/IKsiPublicationsFileServiceProtocol.cs
@@ -22,7 +22,7 @@ using System;
 namespace Guardtime.KSI.Service
 {
     /// <summary>
-    ///     KSI service protocol interface.
+    ///     Service protocol interface for making KSI publications file request.
     /// </summary>
     public interface IKsiPublicationsFileServiceProtocol
     {
