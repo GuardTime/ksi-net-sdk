a4d4b012acd73f42505b462f78cba6fa37c601bf
tarmopaavel <tarmo.paavel@guardtime.com>
2018-02-19 12:23:39 +0200

    Changed version number to 3.10

--- ksi-net-api-test/Properties/AssemblyInfo.cs
+++ ksi-net-api-test/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.9.369.0")]
-[assembly: AssemblyFileVersion("3.9.369.0")]
+[assembly: AssemblyVersion("3.10.369.0")]
+[assembly: AssemblyFileVersion("3.10.369.0")]
