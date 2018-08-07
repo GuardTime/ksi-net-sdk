cd448b38969247f6c1e513806544974825297e63
tarmopaavel <tarmo.paavel@guardtime.com>
2018-08-07 12:03:08 +0300

    Changed version number to 3.11

--- ksi-net-api-crypto-bouncycastle/Properties/AssemblyInfo.cs
+++ ksi-net-api-crypto-bouncycastle/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.10.524.0")]
-[assembly: AssemblyFileVersion("3.10.524.0")]
+[assembly: AssemblyVersion("3.11.524.0")]
+[assembly: AssemblyFileVersion("3.11.524.0")]
